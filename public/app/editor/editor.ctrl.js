b8d8d85dfdf2de5f27d6e9269c8a7e63aa951d81
arturomc <4rturo.m.c@gmail.com>
2015-10-15 14:47:33 -0500

    init

--- public/app/editor/editor.ctrl.js
+++ public/app/editor/editor.ctrl.js
@@ -3,7 +3,24 @@
 
     angular
         .module("app")
-        .controller("editorCtrl", function($scope){
+        .controller("editorCtrl", editorCtrl);
 
-        })
+    editorCtrl.$inject = ["$scope"];
+    function editorCtrl($scope) {
+
+        $scope.someText = "GET _query {}";
+        $scope.editorContext ;
+        $scope.aceOptions = {
+            theme: "monokai",
+
+            onLoad: function (_ace) {
+                $scope.editorContext = _ace.getSession();
+                $scope.editorContext.setMode("ace/mode/acense");
+                console.log("loading stuff")
+            },
+            onChange: function (_ace) {
+                console.log("changing stuff");
+            }
+        }
+    }
 })();
